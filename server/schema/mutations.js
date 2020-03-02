9bd754a97bed148b96e53b9207a8574d94ad0570
Garon Hock <garonhock@yahoo.com>
2020-03-01 22:16:34 -0500

    Adds backend user authentication

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -1,10 +1,55 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLID } = graphql;
 const mongoose = require("mongoose");
+const UserType = require("./types/user_type")
+require("../../models/index");
+const AuthService = require("../services/Auth");
+
 
 const mutation = new GraphQLObjectType({
   name: "Mutation",
-  fields: {}
+  fields: {
+    register: {
+      type: UserType,
+      args: {
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(_, args) {
+        return AuthService.register(args);
+      }
+    }
+  },
+  logout: {
+    type: UserType,
+    args: {
+      // all we need to log the user our is an id
+      _id: { type: GraphQLID }
+    },
+    resolve(_, args) {
+      return AuthService.logout(args);
+    }
+  },
+  login: {
+    type: UserType,
+    args: {
+      email: { type: GraphQLString },
+      password: { type: GraphQLString }
+    },
+    resolve(_, args) {
+      return AuthService.login(args);
+    }
+  },
+  verifyUser: {
+    type: UserType,
+    args: {
+      token: { type: GraphQLString }
+    },
+    resolve(_, args) {
+      return AuthService.verifyUser(args);
+    }
+  }
 });
 
 module.exports = mutation;
