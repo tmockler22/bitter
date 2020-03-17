1d350bbc3932813a23a2673750350f9f7455768b
MichaelPowell <gieringmj@gmail.com>
2020-03-17 04:33:08 -0700

    Change keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY,
-  accessKeyId: process.env.ACCESS_KEY_ID
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID
 }
--- server/services/s3.js
+++ server/services/s3.js
@@ -8,17 +8,14 @@ const singleFileUpload = async file => {
   const { filename, mimetype, createReadStream } = await file;
   const fileStream = createReadStream();
   const path = require("path");
-  // name of the file in your S3 bucket will be the date in ms plus the extension name
   const Key = new Date().getTime().toString() + path.extname(filename);
   const uploadParams = {
-    // name of your bucket here
     Bucket: "bitter-app",
     Key,
     Body: fileStream
   };
   const result = await s3.upload(uploadParams).promise();
 
-  // save the name of the file in your bucket as the key in your database to retrieve for later
   return result.Key;
 };
 
