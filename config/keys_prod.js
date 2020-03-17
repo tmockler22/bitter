3d58f7dc30c500143ca57b7ea26690be015191de
MichaelPowell <gieringmj@gmail.com>
2020-03-17 04:00:38 -0700

    Fix Mongo URI

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
+  MONGO_URI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
   accessKeyId: process.env.ACCESS_KEY_ID
