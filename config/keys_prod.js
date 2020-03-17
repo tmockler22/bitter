2a7502133832716f8584eca1418e9d438d94a55f
MichaelPowell <gieringmj@gmail.com>
2020-03-17 04:47:13 -0700

    Fix keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,6 +1,4 @@
 module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID
 }
